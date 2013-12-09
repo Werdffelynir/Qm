da6677728e81982bfbbc70afaf36d9997225c8aa
Oleg Knysh <ko@cosmonova.net>
2013-12-09 19:46:30 +0200

    -

---
+++ app/Classes/QmAjax.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Comp-2
+ * Date: 09.12.13
+ * Time: 19:35
+ * To change this template use File | Settings | File Templates.
+ */
+
+class QmAjax {
+
+
+
+
+    /**
+     *
+     * Метод отправки формы через AJAX для случаев когда конструкциями Yii невозможно
+     * справиться.
+     * <pre>
+        $ajax = formAJAX("idForm", "tickets/sendmsg",
+            array(
+                    'success'   =>'function (data) { console.log(data); }',
+                    'beforeSend'=>'function (data) { console.log(data); }',
+                    'error'     =>'function (data) { console.log(data); }',
+                )
+            );
+     * </pre>
+     * @param $idForm
+     * @param $url
+     * @param $functions
+     * @return string
+     */
+    public static function formPostAJAX($idForm, $url, $functions){
+        $javascript = '
+                <script type="text/javascript">
+                        $("#'.$idForm.'").submit(function () {
+                            var url = "'.URL.'/'.$url.'";
+                            $.ajax({
+                                type: "POST",
+                                url: url,
+                                data: $("#'.$idForm.'").serialize()';
+        $javascript .= ($functions['success'])      ? ', success: '.$functions['success'] : null;
+        $javascript .= ($functions['beforeSend'])   ? ', beforeSend: '.$functions['beforeSend'] : null;
+        $javascript .= ($functions['error'])        ? ', success: '.$functions['error'] : null;
+        $javascript .= '});
+                            return false;
+                        });
+                </script>
+            ';
+        return $javascript;
+    }
+
+
+    /**
+     *
+     * Еквевалентен предведущему но берет больше параметров
+     *
+    $ajax = formAJAX("idForm", "tickets/sendmsg",
+        array(
+            'type'      =>'GET',
+            'url'       =>'http://other/other/',
+            'dataType'  =>'json',
+            'data'      =>'{param: data1, param2: data2 }',
+            'success'   =>'function (data) { console.log(data); }',
+            'beforeSend'=>'function (data) { console.log(data); }',
+            'error'     =>'function (data) { console.log(data); }',
+        )
+    );
+     *
+     * @param $idForm
+     * @param $url
+     * @param $functions
+     * @return string
+     */
+    function formAJAX($idForm, $url, $functions){
+        $javascript = '
+        <script type="text/javascript">
+                $("#'.$idForm.'").submit(function () {
+                    var url = "'.URL.'/'.$url.'";
+                    $.ajax({';
+        $javascript .= ($functions['type'])         ? 'type: "'.$functions['type'].'"' : 'type: "POST",';
+        $javascript .= ($functions['url'])          ? ', url: "'.$functions['url'].'"' : 'url: url';
+        $javascript .= ($functions['dataType'])     ? ', dataType: "'.$functions['dataType'].'"' : null;
+        $javascript .= ($functions['data'])         ? ', data: "'.$functions['data'].'"' : 'data: $("'.$idForm.'").serialize()';
+        $javascript .= ($functions['success'])      ? ', success: '.$functions['success'] : null;
+        $javascript .= ($functions['beforeSend'])   ? ', beforeSend: '.$functions['beforeSend'] : null;
+        $javascript .= ($functions['error'])        ? ', error: '.$functions['error'] : null;
+        $javascript .= '});
+                    return false;
+                });
+        </script>
+        ';
+        return $javascript;
+    }
+
+
+
+
+
+
+
+
+
+}
--- app/Classes/QmFunc.php
+++ app/Classes/QmFunc.php
@@ -5,6 +5,17 @@
 
 class QmFunc {
 
+
+
+
+
+    /** ********************************************************************************************************
+     **
+     **                                         REQUEST & URL
+     **
+     ********************************************************************************************************** */
+
+
     /**
      * Перенаправление по указаному роуту, относитльный URL. Метод настраивает ответ с кодом HTTP и время задержки.
      * Если время задержки задано, эта функция всегда будет возвращать логическое TRUE,
@@ -52,5 +63,118 @@ class QmFunc {
         echo "</body></html>";
         exit;
     }
+
+
+
+
+
+    /** ********************************************************************************************************
+    **
+    **                                         STRING FUNCTIONS
+    **
+    ********************************************************************************************************** */
+
+
+
+
+    /**
+     * Функция переобразования строк
+     *
+     * @param $string
+     * @param $format
+     *      MB_CASE_UPPER   - В верхний регистр
+     *      MB_CASE_LOWER   - В нижний регистр
+     *      MB_CASE_TITLE   - Первый символ каждого слова в верхний регистр
+     *      UPPER           - В верхний регистр
+     *      LOWER           - В нижний регистр
+     *      UPPER_FIRST     - В верхний регистр первый символ строки. (по умолчанию)
+     *      UPPER_FIRST_E   - В верхний регистр первый символ строки? каждого приложения.
+     * @return bool|string
+     */
+
+    public static function convertStr($string, $format="UPPER_FIRST"){
+
+        switch($format)
+        {
+            case "MB_CASE_UPPER":
+                $string = mb_convert_case($string, MB_CASE_UPPER, "UTF-8");
+                break;
+            case "MB_CASE_LOWER":
+                $string = mb_convert_case($string, MB_CASE_LOWER, "UTF-8");
+                break;
+            case "MB_CASE_TITLE":
+                $string = mb_convert_case($string, MB_CASE_TITLE, "UTF-8");
+                break;
+            case "UPPER":
+                $string = mb_strtoupper($string, "UTF-8");
+                break;
+            case "LOWER":
+                $string = mb_strtolower($string, "UTF-8");
+                break;
+            case "UPPER_FIRST":
+                preg_match_all("|^(.)(.*)$|u", self::convertStr($string, "MB_CASE_LOWER"), $strArr);
+                return mb_strtoupper($strArr[1][0], "UTF-8").$strArr[2][0];
+                break;
+            case "UPPER_FIRST_E":
+                $tempStr = explode(".", $string);
+                $tempJoin = '';
+                foreach ($tempStr as $value) {
+                    $tempJoin .= self::convertStr(trim($value), "UPPER_FIRST").". ";
+                }
+                $string = mb_substr($tempJoin, 0, -3);
+                break;
+            default:
+                $string = false;
+        }
+
+        return $string;
+    }
+
+
+    /**
+     * Обрезать строку по количеству слов.
+     *
+     * @param $string строка для обработки
+     * @param $maxLen количество слов
+     * @param string $more окончание more '...'
+     * @param $linkMore ссылка на more
+     * @param bool $encode strip_tags()
+     * @return string обрезаная строка
+     *
+     */
+    public static function cutText($string, $maxLen, $more='...', $linkMore=null, $encode=true)
+    {
+        if($encode==true)
+        {
+            $string = strip_tags($string);
+            $string = trim(preg_replace('~\s+~s', ' ', $string));
+        }
+
+        if(!is_null($linkMore))
+            $more = '<a href="'.$linkMore.'">'.$more.'</a>';
+
+        $words = explode(' ', $string);
+
+        if ($maxLen < 1 || sizeof($words) <= $maxLen) {
+            return $string;
+        }
+
+        $words = array_slice($words, 0, $maxLen);
+
+        $out = implode(' ', $words);
+
+        return $out.' '.$more;
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }
