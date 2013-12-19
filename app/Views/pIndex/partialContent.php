199715766c3cfcde1747845839c3282ac3d08302
Oleg Knysh <ko@cosmonova.net>
2013-12-19 19:08:10 +0200

    doc info files

--- app/Views/pIndex/partialContent.php
+++ app/Views/pIndex/partialContent.php
@@ -32,31 +32,74 @@
 </p>
 <div class="">
 <pre><code>
-[app]               Установленные приложения
-··· [Classes]       Дополнительные расширения <a href="#">Детальней</a>.
-··· ··· Qm.php      Класс Расширения, всегда включен в дистрибютив <a href="#">Что в нем</a> и <a href="#">другие</a>.
-··· [Controllers]   Директория контролеров (логика). <a href="#">В документацию</a>.
-··· [Models]        Директория моделей (работа с БД). <a href="#">В документацию</a>.
-··· [Views]         Виды преложения. <a href="#">Работа с видами</a>.
-[css]
-[fonts]
-[img]
-[js]
-[lib]               системная часть (ядро) фреймворка <a href="#">link</a>
-··· [database]      кталог с файлами базы-данных SQLite и Файловой БД
-··· [generator]     генератор кода
-··· App.php         ooo
-··· ClLoader.php    ooo
-··· Controller.php  ooo
-··· Model.php       ooo
-··· DataBase.php    ooo
-··· config.php      ooo
-··· functions.php   ooo
-[theme]             шаблоны layout
-··· [qmTheme]
-index.php           прием и разбор всех HTTP-запросов
+APPLICATION                         <span class="commentos">Корень фреймворка</span>
+¦
+¦   .htaccess                       <span class="commentos">.htaccess</span>
+¦   index.php                       <span class="commentos">Вхождение</span>
+¦
++---app                             <span class="commentos">Установленные приложения</span>
+¦   ¦   bootstrap.php               <span class="commentos">__OOO__</span>
+¦   ¦   configuration.php           <span class="commentos">__OOO__</span>
+¦   ¦   functions.php               <span class="commentos">__OOO__</span>
+¦   ¦
+¦   +---Classes                     <span class="commentos">Классы для дополнительного расширения <a href="#">Детальней</a>.</span>
+¦   +---Controllers                 <span class="commentos">Логика преложения. <a href="#">В документацию</a>.</span>
+¦   ¦       ControllerIndex.php     <span class="commentos">Основной контрелер.</span>
+¦   ¦
+¦   +---Models                      <span class="commentos">Логика работы с БД SQL запросов <a href="#">В документацию</a>.</span>
+¦   +---Structure                   <span class="commentos">Структуры модулей</span>
+¦   ¦   L---administrator           <span class="commentos">Например. Сруктура адмиен части преложения <a href="#">В документацию</a>.</span>
+¦   ¦       +---Controllers         <span class="commentos">...</span>
+¦   ¦       +---Models              <span class="commentos">...</span>
+¦   ¦       L---Views               <span class="commentos">...</span>
+¦   ¦
+¦   +---Views                       <span class="commentos">Представления или виды преложения. <a href="#">Работа с видами</a>.</span>
+¦   ¦   L---main.php                <span class="commentos">Основной вид.</span>
+¦   ¦
+¦   L---_protected                  <span class="commentos">Каталог для необходимых дополнительных файлов.</span>
+¦       ¦   AppConfig.php           <span class="commentos">Напр. файл конфигурации.</span>
+¦       ¦
+¦       L---DATABASE                <span class="commentos">Или БД</span>
+¦               QmDataBase.sqlite   <span class="commentos"></span>
+¦
++---lib                             <span class="commentos">Системная часть (ядро) фреймворка <a href="#">В документацию</a></span>
+¦   ¦   bootstrap.php               <span class="commentos">Запус всех гнеобходимых частей</span>
+¦   ¦   configuration.php           <span class="commentos">Базовая и системная конфигурация преложения и фреймворка.</span>
+¦   ¦   functions.php               <span class="commentos">Системные процедурные функции.</span>
+¦   ¦
+¦   +---Abstracts                   <span class="commentos">Каталог базовых абстрактных классов.</span>
+¦   ¦       Base.php                <span class="commentos">Абстрактный базовый файл.</span>
+¦   ¦       Controller.php          <span class="commentos">Абстрактный контролер.</span>
+¦   ¦       Model.php               <span class="commentos">Абстрактная модель.</span>
+¦   ¦
+¦   +---Classes                     <span class="commentos">Системные Классы расширения</span>
+¦   ¦       ClLoader.php            <span class="commentos"></span>
+¦   ¦       SafePDO.php             <span class="commentos"></span>
+¦   ¦       SimplePDO.php           <span class="commentos"></span>
+¦   ¦
+¦   +---Core                        <span class="commentos">Директория ядра</span>
+¦   ¦   ¦   App.php                 <span class="commentos">Файл ядра</span>
+¦   ¦   ¦
+¦   ¦   L---Gen                     <span class="commentos">Генератор кода</span>
+¦   ¦
+¦   L---DATABASE                    <span class="commentos">Каталог с файлами базы-данных SQLite и Файловой БД</span>
+¦           QmDataBase.sqlite       <span class="commentos">БД SQLite</span>
+¦
+L---theme                           <span class="commentos">Директория шаблонов layout (темы)</span>
+¦   error404.php                    <span class="commentos"></span>
+¦
++---defaultTheme                    <span class="commentos">Тема по умолчанию.</span>
+¦   ¦   main.php                    <span class="commentos">Фреймворк запускает файл main.php темы.</span>
+¦   ¦
+¦   +---css                         <span class="commentos"></span>
+¦   +---fonts                       <span class="commentos"></span>
+¦   L---js                          <span class="commentos"></span>
+¦
+L---qmTheme                         <span class="commentos">Может размежать множество тем.</span>
 
-</code></pre>
+
+
+    </code></pre>
 </div>
 
 <h2>Разработка.</h2>
