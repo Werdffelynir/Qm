9b41648064f5656fb66f219e745e2c66070c0234
Oleg Knysh <ko@cosmonova.net>
2013-12-25 18:30:45 +0200

    some updates

---
+++ info.php
@@ -0,0 +1,49 @@
+<?php
+	
+/*
+Next update
+- переписать для ускорения ядро системы
+- use traits as Расширения для классов Контролеров
+- use namespace 
+- use new type array []
+- use __set() & __set AND __isset() __unset()
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ system/bootstrap.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * MVC PHP Framework Quick Minimalism
+ * File:    bootstrap.php
+ * Version: 0.2.0
+ * Author:  OL Werdffelynir
+ * Date:    07.12.13
+ */
+
+
+/** Hard-Core system */
+include "Core/App.php";
+
+/** Autoloader app classes*/
+include "Classes/ClLoader.php";
+
+/** Подключение класса работы с БД */
+if(QmConf("db")){
+    $classDB = QmConf("db");
+    include "Classes/SafePDO.php";
+    include "Classes/".$classDB['class'].".php";
+}
+
+/** Abstracted class Base class, uses Controller, Model */
+include "Abstracts/Base.php";
+
+/** Подключение внутрених файлов Преложения */
+if(file_exists(PATH.QmConf("pathApp").DS."functions.php"))
+    include PATH.QmConf("pathApp").DS."functions.php";
+if(file_exists(PATH.QmConf("pathApp").DS."bootstrap.php"))
+    include PATH.QmConf("pathApp").DS."bootstrap.php";
+    
+/** Abstracted class Controller*/
+include "Abstracts/Controller.php";
+
+/** Abstracted class Model */
+include "Abstracts/Model.php";
+
